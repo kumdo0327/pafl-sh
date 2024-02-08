d10685869f3fafd23c5fdfd4ef7504b39ea82fba
kumdo0327 <kumdo0327@naver.com>
2024-02-08 16:21:44 +0900

    update

--- run_py.sh
+++ run_py.sh
@@ -2,13 +2,13 @@
 
 if [ $# -eq 1 ];
 then
-    ~/PAFL/PAFL/main -p black -l python -m $1 -v 1-14,16-20,23 -d /4tb/donguk/BugsInPy/black -t /4tb/donguk/BugsInPy/black -i ~/PAFL/oracle
+    ~/PAFL/PAFL/main -p black -l python -m $1 -v 23,19-17,12,10,7-3,1 -d /4tb/donguk/BugsInPy/black -t /4tb/donguk/BugsInPy/black -i ~/PAFL/oracle
 elif [ $# -eq 2 ];
 then
-    ~/PAFL/PAFL/main -p black -l python -m $1 -v 1-14,16-20,23 -d /4tb/donguk/BugsInPy/black -t /4tb/donguk/BugsInPy/black -i ~/PAFL/oracle $2
+    ~/PAFL/PAFL/main -p black -l python -m $1 -v 23,19-17,12,10,7-3,1 -d /4tb/donguk/BugsInPy/black -t /4tb/donguk/BugsInPy/black -i ~/PAFL/oracle $2
 elif [ $# -eq 3 ];
 then
-    ~/PAFL/PAFL/main -p black -l python -m $1 -v 1-14,16-20,23 -d /4tb/donguk/BugsInPy/black -t /4tb/donguk/BugsInPy/black -i ~/PAFL/oracle $2 $3
+    ~/PAFL/PAFL/main -p black -l python -m $1 -v 23,19-17,12,10,7-3,1 -d /4tb/donguk/BugsInPy/black -t /4tb/donguk/BugsInPy/black -i ~/PAFL/oracle $2 $3
 else
     exit 1
 fi
