f4ff1a7ebfcab406f0b1245df1ceffad176c49f0
kumdo0327 <kumdo0327@naver.com>
2024-02-19 20:25:21 +0900

    update

--- run_py.sh
+++ run_py.sh
@@ -7,10 +7,11 @@ then
 elif [ $# -eq 2 ];
 then
     ~/PAFL/PAFL/main -p thefuck -l python -m $1 -v 32-13.11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $2
+    ~/PAFL/PAFL/main -p fastapi -l python -m $1 -v 15-1 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $2
 elif [ $# -eq 3 ];
 then
     ~/PAFL/PAFL/main -p thefuck -l python -m $1 -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $2 $3
+    ~/PAFL/PAFL/main -p fastapi -l python -m $1 -v 15-1 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $2 $3
 
 else
     exit 1
-fi
