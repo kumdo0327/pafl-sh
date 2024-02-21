9135b99c9fbd492d687be160be65f0d5a53570a7
kumdo0327 <kumdo0327@naver.com>
2024-02-21 14:28:34 +0900

    update

--- run_py.sh
+++ run_py.sh
@@ -4,14 +4,16 @@ if [ $# -eq 1 ];
 then
     ~/PAFL/PAFL/main -p thefuck -l python -m $1 -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle
     ~/PAFL/PAFL/main -p fastapi -l python -m $1 -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle
+    ~/PAFL/PAFL/main -p spacy -l python -m $1 -v 10-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle
 elif [ $# -eq 2 ];
 then
     ~/PAFL/PAFL/main -p thefuck -l python -m $1 -v 32-13.11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $2
     ~/PAFL/PAFL/main -p fastapi -l python -m $1 -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $2
+    ~/PAFL/PAFL/main -p spacy -l python -m $1 -v 10-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle $2
 elif [ $# -eq 3 ];
 then
     ~/PAFL/PAFL/main -p thefuck -l python -m $1 -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $2 $3
     ~/PAFL/PAFL/main -p fastapi -l python -m $1 -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $2 $3
-
+    ~/PAFL/PAFL/main -p spacy -l python -m $1 -v 10-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle $2 $3
 else
     exit 1
