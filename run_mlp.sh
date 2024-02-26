626b48ead8f719b028b936d49da391ab0d8fa6a9
kumdo0327 <kumdo0327@naver.com>
2024-02-26 22:27:14 +0900

    update

--- run_mlp.sh
+++ run_mlp.sh
@@ -7,6 +7,8 @@ then
     ~/PAFL/PAFL/main -p libchewing -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 6,8,3-5,7,2,1 -d /4tb/donguk/bugscpp/libchewing -t /4tb/donguk/bugscpp/test_libchewing -i ~/PAFL/oracle
     ~/PAFL/PAFL/main -p libxml2 -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 4,7,1-3,5,6 -d /4tb/donguk/bugscpp/libxml2 -t /4tb/donguk/bugscpp/test_libxml2 -i ~/PAFL/oracle
     ~/PAFL/PAFL/main -p yaml_cpp -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 10,1-9 -d /4tb/donguk/bugscpp/yaml_cpp -t /4tb/donguk/bugscpp/test_yaml_cpp -i ~/PAFL/oracle
+    ~/PAFL/PAFL/main -p thefuck -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle
+    ~/PAFL/PAFL/main -p fastapi -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle
 elif [ $# -eq 1 ];
 then
     ~/PAFL/PAFL/main -p cpp_peglib -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 8,9,7,6,5,4,2,3,1,10 -d /4tb/donguk/bugscpp/cpp_peglib -t /4tb/donguk/bugscpp/test_cpp_peglib -i ~/PAFL/oracle $1
@@ -14,6 +16,8 @@ then
     ~/PAFL/PAFL/main -p libchewing -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 6,8,3-5,7,2,1 -d /4tb/donguk/bugscpp/libchewing -t /4tb/donguk/bugscpp/test_libchewing -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p libxml2 -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 4,7,1-3,5,6 -d /4tb/donguk/bugscpp/libxml2 -t /4tb/donguk/bugscpp/test_libxml2 -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p yaml_cpp -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 10,1-9 -d /4tb/donguk/bugscpp/yaml_cpp -t /4tb/donguk/bugscpp/test_yaml_cpp -i ~/PAFL/oracle $1
+    ~/PAFL/PAFL/main -p thefuck -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $1
+    ~/PAFL/PAFL/main -p fastapi -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $1
 elif [ $# -eq 2 ];
 then
     ~/PAFL/PAFL/main -p cpp_peglib -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 8,9,7,6,5,4,2,3,1,10 -d /4tb/donguk/bugscpp/cpp_peglib -t /4tb/donguk/bugscpp/test_cpp_peglib -i ~/PAFL/oracle $1 $2
@@ -21,6 +25,8 @@ then
     ~/PAFL/PAFL/main -p libchewing -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 6,8,3-5,7,2,1 -d /4tb/donguk/bugscpp/libchewing -t /4tb/donguk/bugscpp/test_libchewing -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p libxml2 -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 4,7,1-3,5,6 -d /4tb/donguk/bugscpp/libxml2 -t /4tb/donguk/bugscpp/test_libxml2 -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p yaml_cpp -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 10,1-9 -d /4tb/donguk/bugscpp/yaml_cpp -t /4tb/donguk/bugscpp/test_yaml_cpp -i ~/PAFL/oracle $1 $2
+    ~/PAFL/PAFL/main -p thefuck -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $1 $2
+    ~/PAFL/PAFL/main -p fastapi -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $1 $2
 else
     exit 1
 fi
