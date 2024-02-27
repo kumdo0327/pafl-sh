857da14bf78cb7270b04e0962929d69bbec45e03
kumdo0327 <kumdo0327@naver.com>
2024-02-27 22:51:20 +0900

    update

--- run_sbfl.sh
+++ run_sbfl.sh
@@ -13,8 +13,8 @@ then
     sh $pafl_sh_path/run_py.sh ochiai,dstar,barinel $1
 elif [ $# -eq 2 ];
 then
-    sh $pafl_sh_path/run_cpp.sh $1 $2
-    sh $pafl_sh_path/run_py.sh $1 $2
+    sh $pafl_sh_path/run_cpp.sh ochiai,dstar,barinel $1 $2
+    sh $pafl_sh_path/run_py.sh ochiai,dstar,barinel $1 $2
 else
     exit 1
 fi
