8d3e880d53edc0011e532fe7609e50c662685ea1
kumdo0327 <kumdo0327@naver.com>
2024-02-27 14:38:22 +0900

    update

--- run_dlfl.sh
+++ run_dlfl.sh
@@ -5,15 +5,18 @@ echo "$pafl_sh_path"
 
 if [ $# -eq 0 ];
 then
-    sh $pafl_sh_path/run_cnn_rnn.sh
+    sh $pafl_sh_path/run_cnn.sh
+    sh $pafl_sh_path/run_rnn.sh
     sh $pafl_sh_path/run_mlp.sh
 elif [ $# -eq 1 ];
 then
-    sh $pafl_sh_path/run_cnn_rnn.sh $1
+    sh $pafl_sh_path/run_cnn.sh $1
+    sh $pafl_sh_path/run_rnn.sh $1
     sh $pafl_sh_path/run_mlp.sh $1
 elif [ $# -eq 2 ];
 then
-    sh $pafl_sh_path/run_cnn_rnn.sh $1 $2
+    sh $pafl_sh_path/run_cnn.sh $1 $2
+    sh $pafl_sh_path/run_rnn.sh $1 $2
     sh $pafl_sh_path/run_mlp.sh $1 $2
 else
     exit 1
