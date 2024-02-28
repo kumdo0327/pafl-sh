f25866f6e2491cc5d78e12915ed37006e403033f
kumdo0327 <kumdo0327@naver.com>
2024-02-28 14:30:51 +0900

    update

--- run_aeneas.sh
+++ run_aeneas.sh
@@ -14,7 +14,6 @@ then
 elif [ $# -eq 1 ];
 then
     ~/PAFL/PAFL/main -p cpp_peglib -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 8,9,7,6,5,4,2,3,1,10 -d /4tb/donguk/bugscpp/cpp_peglib -t /4tb/donguk/bugscpp/test_cpp_peglib -i ~/PAFL/oracle $1
-    ~/PAFL/PAFL/main -p exiv2 -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 10,15,14,4,13,6,8,9,11,12,7,5,16,17,3,2,1,18-20 -d /4tb/donguk/bugscpp/exiv2 -t /4tb/donguk/bugscpp/test_exiv2 -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p libchewing -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 6,8,3-5,7,2,1 -d /4tb/donguk/bugscpp/libchewing -t /4tb/donguk/bugscpp/test_libchewing -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p libxml2 -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 4,7,1-3,5,6 -d /4tb/donguk/bugscpp/libxml2 -t /4tb/donguk/bugscpp/test_libxml2 -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p yaml_cpp -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 10,1-9 -d /4tb/donguk/bugscpp/yaml_cpp -t /4tb/donguk/bugscpp/test_yaml_cpp -i ~/PAFL/oracle $1
@@ -25,7 +24,6 @@ then
 elif [ $# -eq 2 ];
 then
     ~/PAFL/PAFL/main -p cpp_peglib -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 8,9,7,6,5,4,2,3,1,10 -d /4tb/donguk/bugscpp/cpp_peglib -t /4tb/donguk/bugscpp/test_cpp_peglib -i ~/PAFL/oracle $1 $2
-    ~/PAFL/PAFL/main -p exiv2 -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 10,15,14,4,13,6,8,9,11,12,7,5,16,17,3,2,1,18-20 -d /4tb/donguk/bugscpp/exiv2 -t /4tb/donguk/bugscpp/test_exiv2 -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p libchewing -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 6,8,3-5,7,2,1 -d /4tb/donguk/bugscpp/libchewing -t /4tb/donguk/bugscpp/test_libchewing -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p libxml2 -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 4,7,1-3,5,6 -d /4tb/donguk/bugscpp/libxml2 -t /4tb/donguk/bugscpp/test_libxml2 -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p yaml_cpp -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 10,1-9 -d /4tb/donguk/bugscpp/yaml_cpp -t /4tb/donguk/bugscpp/test_yaml_cpp -i ~/PAFL/oracle $1 $2
