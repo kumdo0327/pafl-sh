16da799c0d1cc1db0e717376868e7aa0c46b06cc
kumdo0327 <kumdo0327@naver.com>
2024-02-28 21:54:48 +0900

    update

--- run_sbfl_fast.sh
+++ run_sbfl_fast.sh
@@ -10,6 +10,7 @@ then
     ~/PAFL/PAFL/main -p thefuck -l python -m ochiai,dstar,barinel -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle
     ~/PAFL/PAFL/main -p fastapi -l python -m ochiai,dstar,barinel -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle
     ~/PAFL/PAFL/main -p spacy -l python -m ochiai,dstar,barinel -v 10-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle
+    ~/PAFL/PAFL/main -p youtube-dl -l python -m ochiai,dstar,barinel -v 35-31,10-8,30,7,29,28,6,27,26,5,25,24,23-18,3,16,2,15,1,13,12,11 -d /4tb/donguk/BugsInPy/youtube-dl -t /4tb/donguk/BugsInPy/youtube-dl -i ~/PAFL/oracle
 elif [ $# -eq 1 ];
 then
     ~/PAFL/PAFL/main -p cpp_peglib -l cpp -m ochiai,dstar,barinel -v 8,9,7,6,5,4,2,3,1,10 -d /4tb/donguk/bugscpp/cpp_peglib -t /4tb/donguk/bugscpp/test_cpp_peglib -i ~/PAFL/oracle $1
@@ -20,6 +21,7 @@ then
     ~/PAFL/PAFL/main -p thefuck -l python -m ochiai,dstar,barinel -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p fastapi -l python -m ochiai,dstar,barinel -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p spacy -l python -m ochiai,dstar,barinel -v 10-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle $1
+    ~/PAFL/PAFL/main -p youtube-dl -l python -m ochiai,dstar,barinel -v 35-31,10-8,30,7,29,28,6,27,26,5,25,24,23-18,3,16,2,15,1,13,12,11 -d /4tb/donguk/BugsInPy/youtube-dl -t /4tb/donguk/BugsInPy/youtube-dl -i ~/PAFL/oracle $1
 elif [ $# -eq 2 ];
 then
     ~/PAFL/PAFL/main -p cpp_peglib -l cpp -m ochiai,dstar,barinel -v 8,9,7,6,5,4,2,3,1,10 -d /4tb/donguk/bugscpp/cpp_peglib -t /4tb/donguk/bugscpp/test_cpp_peglib -i ~/PAFL/oracle $1 $2
@@ -30,6 +32,7 @@ then
     ~/PAFL/PAFL/main -p thefuck -l python -m ochiai,dstar,barinel -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p fastapi -l python -m ochiai,dstar,barinel -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p spacy -l python -m ochiai,dstar,barinel -v 10-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle $1 $2
+    ~/PAFL/PAFL/main -p youtube-dl -l python -m ochiai,dstar,barinel -v 35-31,10-8,30,7,29,28,6,27,26,5,25,24,23-18,3,16,2,15,1,13,12,11 -d /4tb/donguk/BugsInPy/youtube-dl -t /4tb/donguk/BugsInPy/youtube-dl -i ~/PAFL/oracle $1 $2
 else
     exit 1
 fi
