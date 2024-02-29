a682855b8656a3e444ca7db0c39f1251ae6fae74
kumdo0327 <kumdo0327@naver.com>
2024-02-29 12:24:22 +0900

    update

--- run_aeneas.sh
+++ run_aeneas.sh
@@ -10,6 +10,7 @@ then
     ~/PAFL/PAFL/main -p thefuck -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle
     ~/PAFL/PAFL/main -p fastapi -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle
     ~/PAFL/PAFL/main -p spacy -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 10-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle
+    ~/PAFL/PAFL/main -p youtube-dl -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 35-31,10-8,30,7,29,28,6,27,26,5,25,24,23-18,3,16,2,15,1,13,12,11 -d /4tb/donguk/BugsInPy/youtube-dl -t /4tb/donguk/BugsInPy/youtube-dl -i ~/PAFL/oracle
 elif [ $# -eq 1 ];
 then
     ~/PAFL/PAFL/main -p cppcheck -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 7,1,27,2,15,29,14,6,23,5,17,30,24,8,16,25,3,19,9,18,12,11,21,28,13,20,22,4,26,10 -d /4tb/donguk/bugscpp/cppcheck -t /4tb/donguk/bugscpp/test_cppcheck -i ~/PAFL/oracle $1
@@ -20,6 +21,7 @@ then
     ~/PAFL/PAFL/main -p thefuck -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p fastapi -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p spacy -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 10-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle $1
+    ~/PAFL/PAFL/main -p youtube-dl -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 35-31,10-8,30,7,29,28,6,27,26,5,25,24,23-18,3,16,2,15,1,13,12,11 -d /4tb/donguk/BugsInPy/youtube-dl -t /4tb/donguk/BugsInPy/youtube-dl -i ~/PAFL/oracle $1
 elif [ $# -eq 2 ];
 then
     ~/PAFL/PAFL/main -p cppcheck -l cpp -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 7,1,27,2,15,29,14,6,23,5,17,30,24,8,16,25,3,19,9,18,12,11,21,28,13,20,22,4,26,10 -d /4tb/donguk/bugscpp/cppcheck -t /4tb/donguk/bugscpp/test_cppcheck -i ~/PAFL/oracle $1 $2
@@ -30,6 +32,7 @@ then
     ~/PAFL/PAFL/main -p thefuck -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p fastapi -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p spacy -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 10-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle $1 $2
+    ~/PAFL/PAFL/main -p youtube-dl -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 35-31,10-8,30,7,29,28,6,27,26,5,25,24,23-18,3,16,2,15,1,13,12,11 -d /4tb/donguk/BugsInPy/youtube-dl -t /4tb/donguk/BugsInPy/youtube-dl -i ~/PAFL/oracle $1 $2
 else
     exit 1
 fi
