4395460b1ec58f51952a0250ba6e83c8cf081d9c
kumdo0327 <kumdo0327@naver.com>
2024-03-01 19:49:00 +0900

    update

--- run_MLP.sh
+++ run_MLP.sh
@@ -9,7 +9,7 @@ then
     ~/PAFL/PAFL/main -p yaml_cpp -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 10,1-9 -d /4tb/donguk/bugscpp/yaml_cpp -t /4tb/donguk/bugscpp/test_yaml_cpp -i ~/PAFL/oracle
     ~/PAFL/PAFL/main -p thefuck -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle
     ~/PAFL/PAFL/main -p fastapi -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle
-    ~/PAFL/PAFL/main -p spacy -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 10-4 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle
+    ~/PAFL/PAFL/main -p spacy -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 7-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle
     ~/PAFL/PAFL/main -p youtube-dl -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 35-31,10-8,30,7,29,28,6,27,26,5,25,24,23-18,3,16,2,15,1,13,12,11 -d /4tb/donguk/BugsInPy/youtube-dl -t /4tb/donguk/BugsInPy/youtube-dl -i ~/PAFL/oracle
 elif [ $# -eq 1 ];
 then
@@ -20,7 +20,7 @@ then
     ~/PAFL/PAFL/main -p yaml_cpp -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 10,1-9 -d /4tb/donguk/bugscpp/yaml_cpp -t /4tb/donguk/bugscpp/test_yaml_cpp -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p thefuck -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p fastapi -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $1
-    ~/PAFL/PAFL/main -p spacy -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 10-4 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle $1
+    ~/PAFL/PAFL/main -p spacy -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 7-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle $1
     ~/PAFL/PAFL/main -p youtube-dl -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 35-31,10-8,30,7,29,28,6,27,26,5,25,24,23-18,3,16,2,15,1,13,12,11 -d /4tb/donguk/BugsInPy/youtube-dl -t /4tb/donguk/BugsInPy/youtube-dl -i ~/PAFL/oracle $1
 elif [ $# -eq 2 ];
 then
@@ -31,7 +31,7 @@ then
     ~/PAFL/PAFL/main -p yaml_cpp -l cpp -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 10,1-9 -d /4tb/donguk/bugscpp/yaml_cpp -t /4tb/donguk/bugscpp/test_yaml_cpp -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p thefuck -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 32-13,11-1 -d /4tb/donguk/BugsInPy/thefuck -t /4tb/donguk/BugsInPy/thefuck -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p fastapi -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 16-2 -d /4tb/donguk/BugsInPy/fastapi -t /4tb/donguk/BugsInPy/fastapi -i ~/PAFL/oracle $1 $2
-    ~/PAFL/PAFL/main -p spacy -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 10-4 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle $1 $2
+    ~/PAFL/PAFL/main -p spacy -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 7-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle $1 $2
     ~/PAFL/PAFL/main -p youtube-dl -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 35-31,10-8,30,7,29,28,6,27,26,5,25,24,23-18,3,16,2,15,1,13,12,11 -d /4tb/donguk/BugsInPy/youtube-dl -t /4tb/donguk/BugsInPy/youtube-dl -i ~/PAFL/oracle $1 $2
 else
     exit 1
