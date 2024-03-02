671de24760fee11aea707fe1e441e343457e659d
kumdo0327 <kumdo0327@naver.com>
2024-03-02 10:35:33 +0900

    update

--- _temp.sh
+++ _temp.sh
@@ -1,6 +1,4 @@
 #!bin/bash
 
-~/PAFL/PAFL/main -p spacy -l python -m ochiai,dstar,barinel -v 7-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle -cg
-~/PAFL/PAFL/main -p spacy -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 7-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle -cg
-~/PAFL/PAFL/main -p spacy -l python -m ochiai,dstar,barinel -v 7-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle -cg --pafl
-~/PAFL/PAFL/main -p spacy -l python -m MLP-1,MLP-2,MLP-3,MLP-4,MLP-5 -v 7-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle -cg --pafl
+~/PAFL/PAFL/main -p spacy -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 7-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle -cg
+~/PAFL/PAFL/main -p spacy -l python -m aeneas-ochiai,aeneas-dstar,aeneas-barinel -v 7-1 -d /4tb/donguk/BugsInPy/spacy -t /4tb/donguk/BugsInPy/spacy -i ~/PAFL/oracle -cg --pafl
